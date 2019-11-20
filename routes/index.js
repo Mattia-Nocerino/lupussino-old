a8aa50a1312eff39e36dd7d65e9bdafc699008c6
Mattia Nocerino <mattia.nocerino@tdt.it>
2019-11-20 18:02:27 +0100

    Connessione al db di test riuscita

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/db', function(req, res, next) {
+  const { Client } = require('pg');
+
+  const client = new Client({
+    connectionString: 'postgresql://postgres:Alanf0rd@localhost:5432/postgres'
+  });
+  
+  client.connect();
+
+  client.query("SELECT to_char(current_timestamp, 'HH12:MI:SS') as now")
+  .then(r => {
+    console.log(r.rows[0].now);
+    res.render('test', {title: r.rows[0].now, message: 'ciao'})
+  })
+  .catch(e => console.error(e.stack))
+  
+  // client.query('SELECT NOW()', (err, res) => {
+  //   if (err) throw err;
+  //   for (let row of res.rows) {
+  //     console.log(JSON.stringify(row));
+  //   }
+  //   client.end();
+  // });
+});
+
 module.exports = router;
