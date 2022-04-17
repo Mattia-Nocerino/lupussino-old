c7af559b1057de65b307e3cb3cd5638809b33fd4
Mattia Nocerino <nocerino.mattia@gmail.com>
2022-04-17 22:49:46 +0200

    Aggiunto avvocato

--- classes/room.js
+++ classes/room.js
@@ -1,66 +1,134 @@
 var Roles = require('./roles.js');
 
 class Room {
-    constructor(name){
-        this.name = name;
-        this.player_list = [];
-        this.mitomane_riconosce_assassini = true;
-        this.testimoni_si_riconoscono = false;
-        this.cittadini_distinti = true;
-        this.game_started = false;
-        this.vote_ended = false;
-        this.configurazioni = [
-            [""], //0
-            [""], //1
-            ["Cittadino", "Investigatore"], //2
-            ["Cittadino", "Testimone", "Testimone", "Assassino", "Mitomane", "Investigatore"], //3
-            ["Cittadino", "Testimone", "Testimone", "Assassino", "Mitomane", "Investigatore", "Investigatrice"], //4
-            ["Cittadino", "Testimone", "Testimone", "Assassino", "Assassino", "Mitomane", "Investigatore", "Investigatrice"], //5
-            ["Cittadino", "Cittadina", "Testimone", "Testimone", "Assassino", "Assassino", "Mitomane", "Investigatore", "Investigatrice"], //6
-            ["Cittadino", "Cittadina", "Testimone", "Testimone", "Testimone", "Assassino", "Assassino", "Mitomane", "Investigatore", "Investigatrice"], //7
-            ["Cittadino", "Cittadina", "Testimone", "Testimone", "Testimone", "Assassino", "Assassino", "Assassino", "Mitomane", "Investigatore", "Investigatrice"], //8
-            [""], //9
-            [""]  //10
-        ];
+  constructor(name) {
+    this.name = name;
+    this.player_list = [];
+    this.mitomane_riconosce_assassini = true;
+    this.testimoni_si_riconoscono = false;
+    this.cittadini_distinti = true;
+    this.game_started = false;
+    this.vote_ended = false;
+    this.configurazioni = [
+      [''], //0
+      [''], //1
+      ['Cittadino', 'Investigatore'], //2
+      [
+        'Cittadino',
+        'Testimone',
+        'Testimone',
+        'Assassino',
+        'Mitomane',
+        'Investigatore',
+      ], //3
+      [
+        'Cittadino',
+        'Testimone',
+        'Testimone',
+        'Assassino',
+        'Mitomane',
+        'Investigatore',
+        'Investigatrice',
+      ], //4
+      [
+        'Cittadino',
+        'Testimone',
+        'Testimone',
+        'Assassino',
+        'Assassino',
+        'Mitomane',
+        'Investigatore',
+        'Investigatrice',
+      ], //5
+      [
+        'Cittadino',
+        'Cittadina',
+        'Testimone',
+        'Testimone',
+        'Assassino',
+        'Assassino',
+        'Mitomane',
+        'Investigatore',
+        'Investigatrice',
+      ], //6
+      [
+        'Cittadino',
+        'Cittadina',
+        'Testimone',
+        'Testimone',
+        'Avvocato',
+        'Assassino',
+        'Assassino',
+        'Mitomane',
+        'Investigatore',
+        'Investigatrice',
+      ], //7
+      [
+        'Cittadino',
+        'Cittadina',
+        'Testimone',
+        'Testimone',
+        'Avvocato',
+        'Assassino',
+        'Assassino',
+        'Assassino',
+        'Mitomane',
+        'Investigatore',
+        'Investigatrice',
+      ], //8
+      [
+        'Cittadino',
+        'Cittadina',
+        'Testimone',
+        'Testimone',
+        'Testimone',
+        'Avvocato',
+        'Assassino',
+        'Assassino',
+        'Assassino',
+        'Mitomane',
+        'Investigatore',
+        'Investigatrice',
+      ], //9
+      [''], //10
+    ];
+  }
+
+  get player_number() {
+    return this.player_list.filter((x) => x.is_online).length;
+  }
+
+  get owner() {
+    return this.player_list.find((x) => x.is_owner == true);
+  }
+
+  updateCittadiniDistinti(ucd) {
+    if (ucd != this.cittadini_distinti) {
+      this.cittadini_distinti = ucd;
+
+      this.configurazioni.forEach((conf, i) => {
+        conf.forEach((r, j) => {
+          if (ucd) {
+            if (r == 'Cittadino' && j == 1) {
+              this.configurazioni[i][j] = 'Cittadina';
+            }
+          } else {
+            if (r == 'Cittadina') {
+              this.configurazioni[i][j] = 'Cittadino';
+            }
+          }
+        });
+      });
     }
+  }
 
-    get player_number() {
-        return this.player_list.filter(x => x.is_online).length
-    }
+  // assignRoles() {
 
-    get owner() {
-        return this.player_list.find(x => x.is_owner == true);
-    }
+  // }
 
-    updateCittadiniDistinti(ucd) {
-        if (ucd != this.cittadini_distinti) {
-            this.cittadini_distinti = ucd;
-
-            this.configurazioni.forEach((conf, i) => {
-                conf.forEach((r, j) =>  {
-                    if (ucd){
-                        if (r == 'Cittadino' && j == 1) {
-                            this.configurazioni[i][j] = 'Cittadina';
-                        }
-
-                    } else {
-                        if (r == 'Cittadina') {
-                            this.configurazioni[i][j] = 'Cittadino';
-                        }
-                    }
-                });
-            });
-        }
-    }
-    
-    // assignRoles() {
-
-    // }
-
-    // gameStart() {
-    //     this.assignRoles();
-    // }
+  // gameStart() {
+  //     this.assignRoles();
+  // }
 }
 
 module.exports = Room;
-
